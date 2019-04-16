028ab05a3ee314b6e20640a9b44909e3063ebff5
Hoang Trinh <hoang.trinhj@gmail.com>
2019-04-16 18:12:02 +0700

    Add Implementation For Put Order

--- tomox/mongodb.go
+++ tomox/mongodb.go
@@ -1,11 +1,16 @@
 package tomox
 
 import (
+	"errors"
+
 	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 )
 
 type MongoDatabase struct {
-	Session *mgo.Session
+	Session        *mgo.Session
+	collectionName string
+	dbName         string
 }
 
 // Global instance of Database struct for singleton use
@@ -13,6 +18,8 @@ var db *MongoDatabase
 
 // InitSession initializes a new session with mongodb
 func NewMongoDatabase(session *mgo.Session, mongoURL string) (*MongoDatabase, error) {
+	dbName := "tomodex"
+	collection := "orders"
 	if db == nil {
 		if session == nil {
 			// Initialize new session
@@ -24,7 +31,11 @@ func NewMongoDatabase(session *mgo.Session, mongoURL string) (*MongoDatabase, er
 			session = ns
 		}
 
-		db = &MongoDatabase{session}
+		db = &MongoDatabase{
+			Session:        session,
+			collectionName: collection,
+			dbName:         dbName,
+		}
 	}
 
 	return db, nil
@@ -49,7 +60,16 @@ func (m *MongoDatabase) Put(key []byte, val interface{}) error {
 	sc := db.Session.Copy()
 	defer sc.Close()
 
-	err := sc.DB(dbName).C(collection).Insert(val)
+	/* TODO: Refactor this later! ASSUME that val is OrderItem type, val might be TradeItem type in the future */
+	o, ok := val.(*OrderItem)
+
+	if ok == false {
+		return errors.New("val is not OrderItem type")
+	}
+
+	query := bson.M{"hash": o.Hash.Hex()}
+
+	_, err := sc.DB(m.dbName).C(m.collectionName).Upsert(query, o)
 
 	if err != nil {
 		return err
