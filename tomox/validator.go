d17c0d3e45f188db023d25ef116e088ebbf80b01
Tuna <ng.tuna@gmail.com>
2019-06-28 18:14:11 +0700

    fix json marshal

--- tomox/validator.go
+++ tomox/validator.go
@@ -1,13 +1,16 @@
 package tomox
 
+import "github.com/ethereum/go-ethereum/log"
+
 func (tx *TxDataMatch) DecodeOrder() (*OrderItem, error) {
 	order := &OrderItem{}
-	if err := DecodeBytesItem(tx.order, order); err != nil {
+	log.Debug("tx.order", "tx.order", tx.Order)
+	if err := DecodeBytesItem(tx.Order, order); err != nil {
 		return order, err
 	}
 	return order, nil
 }
 
 func (tx *TxDataMatch) GetTrades() []map[string]string {
-	return tx.trades
+	return tx.Trades
 }
