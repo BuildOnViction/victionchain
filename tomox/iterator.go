71742c154dab82a99b8686dc4f5f30eb7c731609
Tuna <ng.tuna@gmail.com>
2019-08-05 15:30:04 +0700

    reuse customized iterator

--- tomox/iterator.go
+++ tomox/iterator.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, Agiletech Viet Nam. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package tomox
 
 // Iterator holding the iterator's state
@@ -34,16 +38,25 @@ func (iterator *Iterator) Next(dryrun bool) bool {
 		iterator.node = left
 		goto between
 	}
-	if iterator.node.Right != nil {
+	if len(iterator.node.Item.Keys.Right) > 0 {
 		iterator.node = iterator.node.Right(iterator.tree, dryrun)
-		for iterator.node.Left != nil {
+		if iterator.node == nil {
+			goto end
+		}
+		for len(iterator.node.Item.Keys.Left) > 0 {
 			iterator.node = iterator.node.Left(iterator.tree, dryrun)
+			if iterator.node == nil {
+				goto end
+			}
 		}
 		goto between
 	}
-	if iterator.node.Parent != nil {
+	if len(iterator.node.Item.Keys.Parent) > 0 {
 		node := iterator.node
-		for iterator.node.Parent != nil {
+		for len(iterator.node.Item.Keys.Parent) > 0 {
+			if iterator.node == nil {
+				goto end
+			}
 			iterator.node = iterator.node.Parent(iterator.tree, dryrun)
 			if iterator.tree.Comparator(node.Key, iterator.node.Key) <= 0 {
 				goto between
@@ -76,16 +89,16 @@ func (iterator *Iterator) Prev(dryrun bool) bool {
 		iterator.node = right
 		goto between
 	}
-	if iterator.node.Left != nil {
+	if iterator.node.Item.Keys.Left != nil {
 		iterator.node = iterator.node.Left(iterator.tree, dryrun)
-		for iterator.node.Right != nil {
+		for iterator.node.Item.Keys.Right != nil {
 			iterator.node = iterator.node.Right(iterator.tree, dryrun)
 		}
 		goto between
 	}
-	if iterator.node.Parent != nil {
+	if iterator.node.Item.Keys.Parent != nil {
 		node := iterator.node
-		for iterator.node.Parent != nil {
+		for iterator.node.Item.Keys.Parent != nil {
 			iterator.node = iterator.node.Parent(iterator.tree, dryrun)
 			if iterator.tree.Comparator(node.Key, iterator.node.Key) >= 0 {
 				goto between
@@ -106,7 +119,7 @@ between:
 // Value returns the current element's value.
 // Does not modify the state of the iterator.
 func (iterator *Iterator) Value() []byte {
-	return iterator.node.Value()
+	return iterator.node.Item.Value
 }
 
 // Key returns the current element's key.
