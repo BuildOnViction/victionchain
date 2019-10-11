2421f30d04193046e627dd0df6ed63713ee8f91f
tran nghia <nghiatran@tomochain.com>
2019-10-11 11:47:29 +0700

    new contract relayer

--- tomox/tomox_state/common.go
+++ tomox/tomox_state/common.go
@@ -2,17 +2,18 @@ package tomox_state
 
 import (
 	"errors"
-	"github.com/ethereum/go-ethereum/common"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
 )
 
 var (
-	EmptyRoot  = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
-	Ask        = "SELL"
-	Bid        = "BUY"
-	Market     = "MO"
-	Limit      = "LO"
-	Cancel     = "CANCELLED"
+	EmptyRoot = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
+	Ask       = "SELL"
+	Bid       = "BUY"
+	Market    = "MO"
+	Limit     = "LO"
+	Cancel    = "CANCELLED"
 )
 
 var EmptyHash = common.Hash{}
@@ -69,14 +70,22 @@ var (
 		"balances": 0,
 	}
 	RelayerMappingSlot = map[string]uint64{
-		"CONTRACT_OWNER":   0,
-		"MaximumRelayers":  1,
-		"MaximumTokenList": 2,
-		"RELAYER_LIST":     3,
-		"OWNER_LIST":       4,
+		"CONTRACT_OWNER":       0,
+		"MaximumRelayers":      1,
+		"MaximumTokenList":     2,
+		"RELAYER_LIST":         3,
+		"RELAYER_COINBASES":    4,
+		"RESIGN_REQUESTS":      5,
+		"RELAYER_ON_SALE_LIST": 6,
+		"RelayerCount":         7,
+		"MinimumDeposit":       8,
 	}
 	RelayerStructMappingSlot = map[string]*big.Int{
-		"_deposit": big.NewInt(0),
-		"_fee":     big.NewInt(1),
+		"_deposit":    big.NewInt(0),
+		"_fee":        big.NewInt(1),
+		"_fromTokens": big.NewInt(2),
+		"_toTokens":   big.NewInt(3),
+		"_index":      big.NewInt(4),
+		"_owner":      big.NewInt(5),
 	}
 )
