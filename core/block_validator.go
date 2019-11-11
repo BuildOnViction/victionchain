BuildOnViction/tomochain-v1

changed import paths from ethereum to tomochain

core/block_validator.go
@@ -18,15 +18,15 @@ package core
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/tomochain/go-tomochain/tomox/tomox_state"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/tomox"
+	"github.com/tomochain/go-tomochain/common"
+	"github.com/tomochain/go-tomochain/consensus"
+	"github.com/tomochain/go-tomochain/core/state"
+	"github.com/tomochain/go-tomochain/core/types"
+	"github.com/tomochain/go-tomochain/log"
+	"github.com/tomochain/go-tomochain/params"
+	"github.com/tomochain/go-tomochain/tomox"
 )
 
 // BlockValidator is responsible for validating block headers, uncles and

core/types.go
@@ -17,12 +17,12 @@
 package core
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/tomox"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/tomochain/go-tomochain/common"
+	"github.com/tomochain/go-tomochain/core/state"
+	"github.com/tomochain/go-tomochain/core/types"
+	"github.com/tomochain/go-tomochain/core/vm"
+	"github.com/tomochain/go-tomochain/tomox"
+	"github.com/tomochain/go-tomochain/tomox/tomox_state"
 	"math/big"
 )
 

tomox/tomox.go
@@ -3,18 +3,18 @@ package tomox
 import (
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/tomochain/go-tomochain/core/types"
+	"github.com/tomochain/go-tomochain/p2p"
+	"github.com/tomochain/go-tomochain/tomox/tomox_state"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 	"math/big"
 	"strconv"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/tomochain/go-tomochain/common"
+	"github.com/tomochain/go-tomochain/core/state"
+	"github.com/tomochain/go-tomochain/log"
+	"github.com/tomochain/go-tomochain/rpc"
 	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/sync/syncmap"
 )

BuildOnViction/victionchain

core/block_validator.go
@@ -18,15 +18,15 @@ package core
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum/consensus/posv"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/ethereum/go-tomochain/consensus/posv"
+	"github.com/ethereum/go-tomochain/tomox/tomox_state"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-tomochain/common"
+	"github.com/ethereum/go-tomochain/consensus"
+	"github.com/ethereum/go-tomochain/core/state"
+	"github.com/ethereum/go-tomochain/core/types"
+	"github.com/ethereum/go-tomochain/log"
+	"github.com/ethereum/go-tomochain/params"
 )
 
 // BlockValidator is responsible for validating block headers, uncles and

core/types.go
@@ -17,11 +17,11 @@
 package core
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/ethereum/go-tomochain/common"
+	"github.com/ethereum/go-tomochain/core/state"
+	"github.com/ethereum/go-tomochain/core/types"
+	"github.com/ethereum/go-tomochain/core/vm"
+	"github.com/ethereum/go-tomochain/tomox/tomox_state"
 	"math/big"
 )
 

tomox/tomox.go
@@ -3,19 +3,19 @@ package tomox
 import (
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/tomox/tomox_state"
+	"github.com/ethereum/go-tomochain/consensus"
+	"github.com/ethereum/go-tomochain/core/types"
+	"github.com/ethereum/go-tomochain/p2p"
+	"github.com/ethereum/go-tomochain/tomox/tomox_state"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 	"math/big"
 	"strconv"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/tomochain/go-tomochain/common"
+	"github.com/tomochain/go-tomochain/core/state"
+	"github.com/tomochain/go-tomochain/log"
+	"github.com/tomochain/go-tomochain/rpc"
 	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/sync/syncmap"
 )