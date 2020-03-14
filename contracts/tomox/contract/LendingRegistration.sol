bfb0da5a0449dbc5775ae32ba743fe3efbde55f8
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-03-14 07:22:35 +0000

    update lending contract

--- contracts/tomox/contract/LendingRegistration.sol
+++ contracts/tomox/contract/LendingRegistration.sol
@@ -5,6 +5,14 @@ contract AbstractRegistration {
     function getRelayerByCoinbase(address) public view returns (uint, address, uint256, uint16, address[] memory, address[] memory);
 }
 
+contract AbstractTOMOXListing {
+    function getTokenStatus(address) public view returns (bool);
+}
+
+contract AbstractTokenTRC21 {
+    function issuer() public view returns (address);
+}
+
 contract Lending {
     
     // @dev collateral = 0x0 => get collaterals from COLLATERALS
@@ -33,36 +41,143 @@ contract Lending {
     address[] public ALL_COLLATERALS;
 
     AbstractRegistration public relayer;
+
+    address public CONTRACT_OWNER;
+
+    address constant private tomoNative = 0x0000000000000000000000000000000000000001;
+
+    AbstractTOMOXListing private TomoXListing;
+
+    modifier contractOwnerOnly() {
+        require(msg.sender == CONTRACT_OWNER, "Contract Owner Only.");
+        _;
+    }
+
+    function indexOf(address[] memory addrs, address target) internal pure returns (bool){
+        for (uint i = 0; i < addrs.length; i ++) {
+            if (addrs[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function termIndexOf(uint256[] memory terms, uint256 target) internal pure returns (bool){
+        for (uint i = 0; i < terms.length; i ++) {
+            if (terms[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
     
     constructor (address r) public {
         relayer = AbstractRegistration(r);
+        CONTRACT_OWNER = msg.sender;
     }
     
-    function addCollateral(address token, uint256 depositRate, uint256 liquidationRate) public {
+    // add/update depositRate liquidationRate price for collateral
+    function addCollateral(address token, uint256 depositRate, uint256 liquidationRate, uint256 price) public contractOwnerOnly {
+        require(depositRate >= 100 && liquidationRate > 100, "Invalid rates");
+        require(depositRate > liquidationRate , "Invalid rates");
+
+        bool b = TomoXListing.getTokenStatus(token) || (token == tomoNative);
+        require(b, "Invalid collateral");
+
         COLLATERAL_LIST[token] = Collateral({
             _depositRate: depositRate,
             _liquidationRate: liquidationRate,
-            _price: 0
+            _price: price
         });
-        COLLATERALS.push(token);
-        ALL_COLLATERALS.push(token);
+
+        if (!indexOf(COLLATERALS, token)) {
+            COLLATERALS.push(token);
+            ALL_COLLATERALS.push(token);
+        }
+    }
+
+    // update price for collateral
+    function setCollateralPrice(address token, uint256 price) public {
+
+        bool b = TomoXListing.getTokenStatus(token) || (token == tomoNative);
+        require(b, "Invalid collateral");
+
+        require(COLLATERAL_LIST[token]._depositRate >= 100, "Invalid collateral");
+
+        if (indexOf(COLLATERALS, token)) {
+            require(msg.sender == CONTRACT_OWNER, "Contract owner required");
+        } else {
+            AbstractTokenTRC21 t = AbstractTokenTRC21(token);
+            require(t.issuer() == msg.sender, "Required token issuer");
+        }
+
+        COLLATERAL_LIST[token]._price = price;
+    }
+
+    function addILOCollateral(address token, uint256 depositRate, uint256 liquidationRate, uint256 price) public {
+        require(depositRate >= 100 && liquidationRate > 100, "Invalid rates");
+        require(depositRate > liquidationRate , "Invalid rates");
+
+        bool b = TomoXListing.getTokenStatus(token);
+        require(b, "Invalid collateral");
+
+        AbstractTokenTRC21 t = AbstractTokenTRC21(token);
+        require(t.issuer() == msg.sender, "Required token issuer");
+        
+        COLLATERAL_LIST[token] = Collateral({
+            _depositRate: depositRate,
+            _liquidationRate: liquidationRate,
+            _price: price
+        });
+
+        if (!indexOf(ALL_COLLATERALS, token)) {
+            ALL_COLLATERALS.push(token);
+        }
     }
     
-    function addBaseToken(address token) public {
-        BASES.push(token);
+    // lending tokens
+    function addBaseToken(address token) public contractOwnerOnly {
+        bool b = TomoXListing.getTokenStatus(token) || (token == tomoNative);
+        require(b, "Invalid base token");
+        if (!indexOf(BASES, token)) {
+            BASES.push(token);
+        }
     }
     
-    function addTerm(uint256 term) public {
-        TERMS.push(term);
+    // period of loan
+    function addTerm(uint256 term) public contractOwnerOnly {
+        require(term >= 60, "Invalid term");
+
+        if (!termIndexOf(TERMS, term)) {
+            TERMS.push(term);
+        }
     }
     
     function update(address coinbase, uint16 tradeFee, address[] memory baseTokens, uint256[] memory terms, address[] memory collaterals) public {
         (, address owner,,,,) = relayer.getRelayerByCoinbase(coinbase);
-        require(owner == msg.sender, "relayer owner required");
-        require(relayer.RESIGN_REQUESTS(coinbase) == 0, "relayer required to close");
-        require(tradeFee >= 1 && tradeFee < 1000, "Invalid Maker Fee"); // 0.01% -> 10%
+        require(owner == msg.sender, "Relayer owner required");
+        require(relayer.RESIGN_REQUESTS(coinbase) == 0, "Relayer required to close");
+        require(tradeFee >= 1 && tradeFee < 1000, "Invalid trade Fee"); // 0.01% -> 10%
         require(baseTokens.length == terms.length, "Not valid number of terms");
         require(baseTokens.length == collaterals.length, "Not valid number of collaterals");
+
+        // validate baseTokens
+        bool b = false;
+        for (uint i = 0; i < baseTokens.length; i++) {
+            b = indexOf(BASES, baseTokens[i]);
+            require(b == true, "Invalid lending token");
+        }
+
+        // validate terms
+        for (i = 0; i < terms.length; i++) {
+            b = termIndexOf(TERMS, terms[i]);
+            require(b == true, "Invalid term");
+        }
+
+        // validate collaterals
+        for (i = 0; i < collaterals.length; i++) {
+            require(indexOf(ALL_COLLATERALS, collaterals[i]), "Not valid number of collaterals");
+        }
         
         LENDINGRELAYER_LIST[coinbase] = LendingRelayer({
             _tradeFee: tradeFee,
