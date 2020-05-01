53490d363ed0acbaccdd2025c4ad5f5d2eb5e530
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-05-01 09:48:50 +0000

    test new multisig trc21

--- contracts/tomox/contract/TRC21.sol
+++ contracts/tomox/contract/TRC21.sol
@@ -1,71 +1,6 @@
 pragma solidity ^0.4.24;
 
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that revert on error
- */
-library SafeMath {
-
-	/**
-	 * @dev Multiplies two numbers, reverts on overflow.
-	 */
-	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-		// Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-		// benefit is lost if 'b' is also tested.
-		// See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-		if (a == 0) {
-			return 0;
-		}
-
-		uint256 c = a * b;
-		require(c / a == b);
-
-		return c;
-	}
-
-	/**
-	 * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
-	 */
-	function div(uint256 a, uint256 b) internal pure returns (uint256) {
-		require(b > 0); // Solidity only automatically asserts when dividing by 0
-		uint256 c = a / b;
-		// assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-		return c;
-	}
-
-	/**
-	* @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-	*/
-	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-		require(b <= a);
-		uint256 c = a - b;
-
-		return c;
-	}
-
-	/**
-	* @dev Adds two numbers, reverts on overflow.
-	*/
-	function add(uint256 a, uint256 b) internal pure returns (uint256) {
-		uint256 c = a + b;
-		require(c >= a);
-
-		return c;
-	}
-
-	/**
-	* @dev Divides two numbers and returns the remainder (unsigned integer modulo),
-	* reverts when dividing by zero.
-		*/
-	function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-		require(b != 0);
-		return a % b;
-	}
-}
-
-
+import "./SafeMath.sol";
 
 /**
  * @title TRC21 interface
@@ -299,7 +234,7 @@ contract TRC21 is ITRC21 {
 }
 
 //Wrap token based on multisig wallet that only mints new token if there are user deposits 
-contract TomoBridgeWrapToken is TRC21 {
+contract MyTRC21 is TRC21 {
     /*
      *  Events
      */
