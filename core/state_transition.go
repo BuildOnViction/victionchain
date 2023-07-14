2eb2792431e27fc3da71322ca7e3665baf1594f4
terryyyz-coin98 <huybd@coin98.finance>
2023-07-14 11:29:16 +0700

    feat: define ExecutionResult

--- core/state_transition.go
+++ core/state_transition.go
@@ -76,6 +76,40 @@ type Message interface {
 	BalanceTokenFee() *big.Int
 }
 
+// message no matter the execution itself is successful or not.
+ type ExecutionResult struct {
+ 	UsedGas    uint64 // Total used gas but include the refunded gas
+ 	Err        error  // Any error encountered during the execution(listed in core/vm/errors.go)
+ 	ReturnData []byte // Returned data from evm(function result or data supplied with revert opcode)
+ }
+
+ // Unwrap returns the internal evm error which allows us for further
+ // analysis outside.
+ func (result *ExecutionResult) Unwrap() error {
+ 	return result.Err
+ }
+
+ // Failed returns the indicator whether the execution is successful or not
+ func (result *ExecutionResult) Failed() bool { return result.Err != nil }
+
+ // Return is a helper function to help caller distinguish between revert reason
+ // and function return. Return returns the data after execution if no error occurs.
+ func (result *ExecutionResult) Return() []byte {
+ 	if result.Err != nil {
+ 		return nil
+ 	}
+ 	return common.CopyBytes(result.ReturnData)
+ }
+
+ // Revert returns the concrete revert reason if the execution is aborted by `REVERT`
+ // opcode. Note the reason can be nil if no data supplied with revert opcode.
+ func (result *ExecutionResult) Revert() []byte {
+ 	if result.Err != vm.ErrExecutionReverted {
+ 		return nil
+ 	}
+ 	return common.CopyBytes(result.ReturnData)
+ }
+
 // IntrinsicGas computes the 'intrinsic gas' for a message with the given data.
 func IntrinsicGas(data []byte, contractCreation, homestead bool) (uint64, error) {
 	// Set the starting gas for the raw transaction
