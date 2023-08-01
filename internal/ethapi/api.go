05621038e646261a193c5d8d57b070b6c7979da9
Dang Nhat Trinh <trinhdn@coin98.finance>
2023-08-01 11:41:05 +0700

    Skip account checks for doCall

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1061,7 +1061,7 @@ func (s *PublicBlockChainAPI) doCall(ctx context.Context, args CallArgs, blockNr
 		GasPrice:          gasPrice,
 		Data:              args.Data,
 		BalanceTokenFee:   balanceTokenFee,
-		SkipAccountChecks: false,
+		SkipAccountChecks: true,
 	}
 
 	// Setup context so it may be cancelled the call has completed
