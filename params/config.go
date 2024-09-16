87039a0e0bc4ad45022d79cdcda98c2af4a84624
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2024-09-16 14:06:56 +0700

    add mainet hard fork block

--- params/config.go
+++ params/config.go
@@ -40,6 +40,7 @@ var (
 		EIP155Block:    big.NewInt(3),
 		EIP158Block:    big.NewInt(3),
 		ByzantiumBlock: big.NewInt(4),
+		SaigonBlock:    big.NewInt(85619448),
 		Posv: &PosvConfig{
 			Period:              2,
 			Epoch:               900,
