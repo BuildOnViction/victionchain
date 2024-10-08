14ca08c7787166d4cfe04da44e47bdaf328eeaaf
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2024-10-08 13:25:16 +0700

    delay timebomb for 14 days

--- params/config.go
+++ params/config.go
@@ -40,7 +40,7 @@ var (
 		EIP155Block:    big.NewInt(3),
 		EIP158Block:    big.NewInt(3),
 		ByzantiumBlock: big.NewInt(4),
-		SaigonBlock:    big.NewInt(85619448),
+		SaigonBlock:    big.NewInt(86158494),
 		Posv: &PosvConfig{
 			Period:              2,
 			Epoch:               900,
