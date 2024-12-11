afb9bde084d3e35c8fd0cc2000d3b9b364c13d85
Khang Nguyen <khangnt@coin98.finance>
2024-12-11 13:51:26 +0700

    fix: improve error handling for unclean shutdown markers and remove unused database version retrieval

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -1,29 +1,6 @@
 package rawdb
 
-import (
-	"github.com/tomochain/tomochain/ethdb"
-	"github.com/tomochain/tomochain/rlp"
-)
-
 var (
-	// databaseVersionKey tracks the current database version.
-	databaseVersionKey = []byte("DatabaseVersion")
-	
 	// uncleanShutdownKey tracks the list of local crashes
 	uncleanShutdownKey = []byte("unclean-shutdown") // config prefix for the db
 )
-
-// ReadDatabaseVersion retrieves the version number of the database.
-func ReadDatabaseVersion(db ethdb.KeyValueReader) *uint64 {
-	var version uint64
-
-	enc, _ := db.Get(databaseVersionKey)
-	if len(enc) == 0 {
-		return nil
-	}
-	if err := rlp.DecodeBytes(enc, &version); err != nil {
-		return nil
-	}
-
-	return &version
-}
