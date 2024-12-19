16ef4e86e04bfc15d243009d9deebf2ab5493c26
Huynh Minh Tri <trihm@coin98.finance>
2024-12-19 11:03:41 +0700

    feat: add prometheus metrics PR #17077

--- metrics/exp/exp.go
+++ metrics/exp/exp.go
@@ -5,6 +5,7 @@ package exp
 import (
 	"expvar"
 	"fmt"
+	"github.com/tomochain/tomochain/metrics/prometheus"
 	"net/http"
 	"sync"
 
@@ -42,6 +43,7 @@ func Exp(r metrics.Registry) {
 	// http.HandleFunc("/debug/vars", e.expHandler)
 	// haven't found an elegant way, so just use a different endpoint
 	http.Handle("/debug/metrics", h)
+	http.Handle("/debug/metrics/prometheus", prometheus.Handler(r))
 }
 
 // ExpHandler will return an expvar powered metrics handler.
