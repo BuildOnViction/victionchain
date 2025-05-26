c6a0d08e0e2e8e9bb293df5d6217c29aeb08a9f2
imteryyy <huybd@coin98.finance>
2025-05-26 15:20:31 +0700

    feat: 🎸 max gas feature

--- contracts/trc21issuer/contract/VRC25Issuer.sol
+++ contracts/trc21issuer/contract/VRC25Issuer.sol
@@ -3,10 +3,18 @@ pragma solidity ^0.4.24;
 import "./TRC21Issuer.sol";
 
 contract VRC25Issuer is TRC21Issuer {
+    mapping(address => uint256) private _maxGasPrice; // if maxGasPrice == 0 then no limit
+
     event Withdraw(address indexed token, address indexed receiver, uint256 value);
 
     constructor(uint256 value) TRC21Issuer(value) public {
         revert("Constructor is not supported, override code to TRC21Issuer address");
+    }
+
+    function updateMaxGasPrice(address token, uint256 maxGasPrice) public {
+        AbstractTokenTRC21 t = AbstractTokenTRC21(token);
+        require(t.issuer() == msg.sender, "Only issuer can update max gas price");
+        _maxGasPrice[token] = maxGasPrice;
     }
 
     function apply(address token) public payable onlyValidCapacity(token) {
@@ -23,5 +31,9 @@ contract VRC25Issuer is TRC21Issuer {
 
         receiver.transfer(amount);
         emit Charge(msg.sender, token, amount);
+    }
+
+    function maxGasPrice(address token) public view returns (uint256) {
+        return _maxGasPrice[token];
     }
 }
